238e0b9a1ab50dd9b7ab6faaecd31eca6af63a35
Administrator <Administrator@SKY-20160517YUZ>
2017-10-06 12:52:55 +0800

    cp表的sort分类调整成字符，产品可同时属于多个分类

--- WebAPI/Controllers/cpController.cs
+++ WebAPI/Controllers/cpController.cs
@@ -51,13 +51,21 @@ namespace WebAPI.Controllers
 
 
         // GET api/values
-        public DataSet Getbysort_id(int sort_id)
+        public DataSet Getbysort_id(string sort_id = "")
         {
-            var sqlstr = "select * from cp where sort_id = " + sort_id + " order by px desc,id desc";
+            var sqlstr = "select * from cp order by px desc,id desc";
+            if (!string.IsNullOrEmpty(sort_id))
+            {
+                sqlstr = "select * from cp where  sort_id like '%%," + sort_id + ",%%' order by px desc,id desc";
+            }
             var result = DataAccess.DataSet(sqlstr, this.Url.Request.RequestUri.Segments[1]);
+
             result.Tables[0].TableName = "cp";
+
             return result;
         }
+
+
 
         // GET api/values
         public DataSet Getbynsort_id(int nsort_id)
